f5c10f337146cf37a8c3b8536541830a3a595565
Ahmed Elgendy <Ahmed.Elgendy@integrant.com>
2023-04-04 05:11:37 +0200

    Add project files.

---
+++ UdemyCourse_DesignPatternsInCSharpAndDotNET/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemyCourse_DesignPatternsInCSharpAndDotNET.Creational_Patterns.Builder;
+using UdemyCourse_DesignPatternsInCSharpAndDotNET.TheSOLIDDesignPrinciples;
+
+namespace UdemyCourse_DesignPatternsInCSharpAndDotNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var a = new StringBuilder();
+            //Console.WriteLine(a);
+            //OpenClosedPrinciple.Execute();
+            //LiskovSubstitutionPrinciple.Execute();
+            //DependencyInversionPrinciple.Execute();
+            HtmlElementDemo.Execute();
+            Console.ReadKey();
+        }
+    }
+}
