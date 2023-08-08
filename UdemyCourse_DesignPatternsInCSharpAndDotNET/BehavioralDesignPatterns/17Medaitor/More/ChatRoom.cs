914a0263acfa2856a9d8757c201a3f09fee6e1e6
Ahmed Elgendy <Ahmed.Elgendy@integrant.com>
2023-08-08 04:13:34 +0300

    Medaitor02

---
+++ UdemyCourse_DesignPatternsInCSharpAndDotNET/BehavioralDesignPatterns/17Medaitor/More/ChatRoom.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace UdemyCourse_DesignPatternsInCSharpAndDotNET.BehavioralDesignPatterns._17Medaitor.More
+{
+    public class ChatRoom
+    {
+        public static void showMessage(User user, String message)
+        {
+            WriteLine(DateTime.Now.ToString() + " [" + user.getName() + "] : " + message);
+        }
+    }
+
+    public class User
+    {
+        private String name;
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public User(String name)
+        {
+            this.name = name;
+        }
+
+        public void sendMessage(String message)
+        {
+            ChatRoom.showMessage(this, message);
+        }
+    }
+
+    public class MediatorPatternDemo
+    {
+        public static void main(String[] args)
+        {
+            User robert = new User("Robert");
+            User john = new User("John");
+
+            robert.sendMessage("Hi! John!");
+            john.sendMessage("Hello! Robert!");
+        }
+    }
+}
